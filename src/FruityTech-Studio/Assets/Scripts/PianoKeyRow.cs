5548e807bce445db46ccec616c553bc7f7192b68
Logan Jordan <ljordan@cocc.edu>
2026-03-09 18:36:10 -0700

    Add active instrument preview

--- src/FruityTech-Studio/Assets/Scripts/PianoKeyRow.cs
+++ src/FruityTech-Studio/Assets/Scripts/PianoKeyRow.cs
@@ -1,7 +1,18 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class PianoKeyRow : MonoBehaviour
+public class PianoKeyRow : MonoBehaviour, IPointerDownHandler
 {
     public string noteId;   
     public int semitone;    
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        var engine = FindFirstObjectByType<SequencerEngine>();
+        if (engine == null)
+            return;
+
+        // Clicking the key list auditions the active instrument at this row.
+        engine.PreviewNoteRow(semitone);
+    }
 }
